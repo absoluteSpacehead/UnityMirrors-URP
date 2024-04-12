65d80ddb32188d8f8158b728a78b319412ea4278
absoluteSpacehead <absoluteSpacehead@gmail.com>
2024-04-12 22:03:19 +0100

    Replace OnPreCull

--- Assets/Scripts/Mirrors/MainCamMirrorHelper.cs
+++ Assets/Scripts/Mirrors/MainCamMirrorHelper.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
+[RequireComponent(typeof(Camera))]
 public class MainCamMirrorHelper : MonoBehaviour
 {
     // Add component to Main Camera to render Mirrors
     Mirror[] mirrors;
+    Camera mCamera;
 
     void Awake()
     {
         mirrors = FindObjectsOfType<Mirror>();
+
+        // Replacement for OnPreCull() which is BIRP only
+        mCamera = GetComponent<Camera>();
+        RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
     }
 
-    void OnPreCull()
+    void OnDestroy()
     {
+        RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
+    }
+
+    void OnBeginCameraRendering(ScriptableRenderContext ctx, Camera cam)
+    {
+        if (cam != mCamera)
+            return;
+
         for (int i = 0; i < mirrors.Length; i++)
         {
             mirrors[i].Render();
